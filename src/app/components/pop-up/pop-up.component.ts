12dc7288dd9848ffd4ae0dcee5ce84b3e4064768
bublik-liquidator <Iris_Gonzales103@mail.ru>
2022-10-28 16:01:51 +0300

    Сейчас работает cale и нет одновременного изменения в admin component

--- src/app/components/pop-up/pop-up.component.ts
+++ src/app/components/pop-up/pop-up.component.ts
@@ -17,10 +17,15 @@ export class PopUpComponent
  
   newCar = new CarData(); 
   cars: CarData[] = [];
-
+  id:string; 
+  name:string; 
+  speed:string; 
   public InitCarPopUP() {        
     this.newCar = this.sharedService.addData();
     this.cars = JSON.parse(localStorage.getItem('cars') || '[]');
+    this.id=this.newCar.id;
+    this.name=this.newCar.name;
+    this.speed=this.newCar.speed;
   }
   saveCarsToStorage(){
     for(let i=0;i<this.cars.length;i++){
@@ -34,24 +39,29 @@ export class PopUpComponent
     // this.cars = this.cars.filter((obj) => obj.id == );
     // localStorage.setItem('cars', JSON.stringify(this.cars));
   }
-  Cale(CAR:CarData){
-    for(let i=0;i<this.cars.length;i++){
-      if(CAR.id==this.cars[i].id){
-        // CAR=this.cars[i];        
-        // this.newCar.name=this.cars[i].name;
-        // this.newCar.speed=this.cars[i].speed;
-        this.newCar=this.cars[i];
+  Cale(){
+    // for(let i=0;i<this.cars.length;i++){
+    //   if(CAR.id==this.cars[i].id){
+        // CAR=this.cars[i];      
+        console.log("ДО .name: " + this.name);  
+         this.name=this.newCar.name;
+         this.speed=this.newCar.speed;
+         console.log(" ПОСЛЕ name: " + this.name);
+         console.log(" ПОСЛЕ this.newCar.name: " + this.newCar.name);
+        //!this.newCar=this.cars[i];
+
         //!! крч проблема в том что оно почнемуто созраняет в сервис 
-        console.log("newCar.name: " + this.newCar.name);
-        console.log("newCar.speed: " + this.newCar.speed);
-        console.log("this.cars[i]: " + this.cars[i]);
-        console.log(" this.cars[i].name: "+this.cars[i].name);
-        console.log(" this.cars[i].speed: " + this.cars[i].speed);
-        console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAA ");
+
+        // console.log("newCar.name: " + this.name);
+        // console.log("newCar.speed: " + this.newCar.speed);
+        // console.log("this.cars[i]: " + this.cars[i]);
+        // console.log(" this.cars[i].name: "+this.cars[i].name);
+        // console.log(" this.cars[i].speed: " + this.cars[i].speed);
+        // console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAA ");
 
       }
-    }  
+    
   }
 
    
-}
+
